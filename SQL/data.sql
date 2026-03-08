7b0263990d66ec6be88b98fb8337c0a1ff8e1b97
Federico <160421165+Fede046@users.noreply.github.com>
2026-03-08 10:07:09 +0100

    start

--- SQL/data.sql
+++ SQL/data.sql
@@ -14,14 +14,33 @@ CREATE TABLE UTENTE (
     Password VARCHAR(30) NOT NULL,
     Luogo VARCHAR(30),
     Data VARCHAR(30)
-) engine=InnoDB;
+);
 
 CREATE TABLE EMAIL (
     Username VARCHAR(30) NOT NULL,
     Indirizzo VARCHAR(255),
     PRIMARY KEY(Username,Indirizzo),
     FOREIGN KEY(Username) REFERENCES UTENTE(Username)
-)
+);
+#Il fatto che un amministratore non può essere anche revisore quando lo metto e dove lo metto?
+CREATE TABLE AMMINISTRATORE(
+    Username VARCHAR(30) NOT NULL,
+    FOREIGN KEY(Username) REFERENCES UTENTE(Username)
+);
+CREATE TABLE REVISORE_ESG(
+    Username VARCHAR(30) NOT NULL,
+    IndiceAffidabilita INT,
+    NumRevisioni INT,
+    FOREIGN KEY(Username) REFERENCES UTENTE(Username),
+    CONSTRAINT CHK_Valutazione CHECK (IndiceAffidabilita >= 1 AND IndiceAffidabilita <= 10)
+);
+CREATE TABLE RESPONSABILE_AZIENDALE(
+    Username VARCHAR(30) NOT NULL,
+    FOREIGN KEY(Username) REFERENCES UTENTE(Username)
+);
+
+CREATE TABLE C
 
 SELECT * FROM UTENTE;
 SELECT Password FROM UTENTE WHERE Username = '814';
+
