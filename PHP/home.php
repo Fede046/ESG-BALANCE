d91fc6977ca0406ac9110d6c69cf3a20808f8fbc
Alexis1513 <ale15siri@gmail.com>
2026-03-09 11:33:40 +0100

    html style

--- PHP/home.php
+++ PHP/home.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../STYLE/style.css">
 </head>
 <body>
-    <a href="login.php"><button>Login</button></a>
-    <a href="registration.php"><button>Registration</button></a>    
+    <h1 class="welcome_page">WELCOME</h1>
+    <a href="login.php" class="welcome_page"><button>Login</button></a>
+    <a href="registration.php" class="welcome_page"><button>Registration</button></a>    
 </body>
 </html>
